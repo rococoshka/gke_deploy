5425705dfe68637a882535cf892182d189f716e8
Aleh Kurcheuski <aleh.kurcheuski@innowise-group.com>
2021-06-18 17:39:43 +0300

    chek webwhooks

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world from Jenkins\n');
+  res.send('Hello world from Jenkins please\n');
 });
 
 app.listen(PORT);
